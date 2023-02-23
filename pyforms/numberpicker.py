4a9e8d044f9b55b41d48bab07263b435aebdb553
kcvinu <kcvinu82@gmail.com>
2023-02-23 08:25:57 +0530

    Cleaned numberpicker module

--- pyforms/numberpicker.py
+++ pyforms/numberpicker.py
@@ -1,22 +1,19 @@
 
-
 # numberpicker module Created on 12-Dec-2022 23:04:20
 
 from ctypes.wintypes import HWND, UINT, HDC
-from ctypes import WINFUNCTYPE, byref, cast, addressof
-import ctypes as ctp
+from ctypes import WINFUNCTYPE, byref, cast
 
-from .control import Control, initCommonCtls
+from .control import Control
 from . import constants as con
 from .commons import MyMessages
-from .enums import ControlType, TextAlignment, ControlDrawMode
+from .enums import ControlType, TextAlignment
 from .events import EventArgs
 from . import apis as api
 from .apis import LRESULT, UINT_PTR, DWORD_PTR, WPARAM, LPARAM, SUBCLASSPROC
 from .colors import Color, clamp
 
-from horology import Timing
-from .winmsgs import log_msg
+# from .winmsgs import log_msg
 
 nump_dict = {}
 nump_tb_dict = {}
@@ -32,14 +29,13 @@ class NumberPicker(Control):
     _count = 1
     __slots__ = ( "_hide_caret", "_track_mouse_leave", "_btn_on_left", "_has_sep", "_top_edge_flag", "_bot_edge_flag",
                     "_auto_rotate", "_min_range", "_max_range", "_value", "_step", "_deci_precis", "_buddy_rect",
-                    "_buddy_style", "_buddy_ex_style", "_buddy_hwnd", "_buddy_cid", "_buddy_subcls_id", "_linex",
-                    "_buddy_subcls_proc", "_txt_pos", "on_value_changed", "_my_rect", "_key_pressed" )
+                    "_buddy_style", "_buddy_ex_style", "_buddy_hwnd", "_buddy_cid", "_buddy_subcls_id", "_linex", "_destroy_count",
+                    "_buddy_subcls_proc", "_txt_pos", "on_value_changed", "_my_rect", "_ud_rect", "_key_pressed" )
 
     def __init__(self, parent, xpos: int = 10, ypos: int = 10, width: int = 80, height: int = 24 ) -> None:
         super().__init__()
         self._cls_name = "msctls_updown32"
-        self.name = f"NumberPicker{NumberPicker._count}"
-        # self._text = self.name if txt == "" else txt
+        self.name = f"NumberPicker_{NumberPicker._count}"
         self._ctl_type = ControlType.NUM_PICKER
         self._parent = parent
         self._bg_color = Color(0xFFFFFF)
@@ -48,17 +44,15 @@ class NumberPicker(Control):
         self._height = height
         self._xpos = xpos
         self._ypos = ypos
-        # self._is_textable = True
         self._style = nump_style
         self._ex_style = 0x00000000
-
         self._btn_on_left = False
         self._max_range = 100
         self._min_range = 0
         self._deci_precis = 0
         self._auto_rotate = False
         self._step = 1
-        self._buddy_style = con.WS_CHILD | con.WS_VISIBLE | con.ES_NUMBER | con.WS_BORDER #|# 0x560100C0
+        self._buddy_style = con.WS_CHILD | con.WS_VISIBLE | con.ES_NUMBER | con.WS_BORDER
         self._buddy_ex_style = con.WS_EX_LEFT | con.WS_EX_LTRREADING
         self._value = self._min_range
         self._txt_pos = TextAlignment.LEFT
@@ -66,12 +60,14 @@ class NumberPicker(Control):
         self._has_sep = False
         self._buddy_rect = api.RECT()
         self._my_rect = api.RECT()
+        self._ud_rect = api.RECT()
         self._track_mouse_leave = False
         self._key_pressed = False
         self._top_edge_flag = con.BF_TOPLEFT
         self._bot_edge_flag = con.BF_BOTTOM
         self._hide_caret = False
         self._linex = 0
+        self._destroy_count = 0
 
         #Events
         self.on_value_changed = 0
@@ -99,6 +95,7 @@ class NumberPicker(Control):
             self._buddy_cid = Control._ctl_id
             self._buddy_subcls_id = Control._subclass_id
             Control._subclass_id += 1
+            if self._btn_on_left: self.width -= 2
             self._buddy_hwnd = api.CreateWindowEx(  self._buddy_ex_style,
                                                     "Edit",
                                                     self._text,
@@ -113,45 +110,26 @@ class NumberPicker(Control):
 
             if self._buddy_hwnd:
                 self._is_created = True
-                # nump_tb_dict[self._buddy_hwnd] = self
+
                 Control._ctl_id += 1
                 api.SetWindowSubclass(self._buddy_hwnd, buddy_wnd_proc, self._buddy_subcls_id, self._hwnd)
                 api.SendMessage(self._buddy_hwnd, con.WM_SETFONT, self.font.handle, 1)
-                api.SendMessage(self._hwnd, con.UDM_SETBUDDY, self._buddy_hwnd, 0)
+                old_buddy = api.SendMessage(self._hwnd, con.UDM_SETBUDDY, self._buddy_hwnd, 0)
                 api.SendMessage(self._hwnd, con.UDM_SETRANGE32, self._min_range, self._max_range)
 
                 api.GetClientRect(self._buddy_hwnd, byref(self._buddy_rect))
-                self._my_rect.left = self._xpos
-                self._my_rect.top = self._ypos
-                self._my_rect.right =  self._xpos + self._width
-                self._my_rect.bottom = self._ypos + self._height
-
-                # Adjust the edit rect for
-                swp_flag = con.SWP_SHOWWINDOW | con.SWP_NOACTIVATE | con.SWP_NOZORDER
-                if self._btn_on_left:
-                    udrc = api.RECT()
-
-                    api.GetClientRect(self._hwnd, byref(udrc))
-                    api.SetWindowPos(self._buddy_hwnd, None,
-                                    self._xpos + udrc.right,
-                                    self._ypos,
-                                    self._buddy_rect.right,
-                                    self._buddy_rect.bottom, swp_flag)
-                else:
-
-                    api.SetWindowPos(self._buddy_hwnd, con.HWND_TOP,
-                                    self._xpos,
-                                    self._ypos,
-                                    self._buddy_rect.right - 1,
-                                    self._buddy_rect.bottom, swp_flag)
-                    self._linex = self._buddy_rect.right - 2
-
+                api.GetClientRect(self._hwnd, byref(self._ud_rect))
+                api.SetRect(byref(self._my_rect), self._xpos, self._ypos, (self._xpos + self._width), (self._ypos + self._height))
                 self._display_value()
+                self._resize_buddy()
+                if old_buddy: api.SendMessage(old_buddy, MyMessages.BUDDY_RESET, 0, 0)
+
 
 
     # -endregion Public funcs
 
     # -region Private funcs
+    # Set number picker style bits
     def _np_set_styles(self):
         if self._btn_on_left:
             self._style ^= con.UDS_ALIGNRIGHT
@@ -165,7 +143,7 @@ class NumberPicker(Control):
             case TextAlignment.CENTER: self._buddy_style |= con.ES_CENTER
             case TextAlignment.RIGHT: self._buddy_style |= con.ES_RIGHT
 
-
+    # Internal function to display value
     def _display_value(self):
         if self._has_sep:
             self._text = f"{self._value:,.{self._deci_precis}f}"
@@ -173,7 +151,7 @@ class NumberPicker(Control):
             self._text = f"{self._value:.{self._deci_precis}f}"
         api.SetWindowText(self._buddy_hwnd, self._text )
 
-
+    # Internal function to calculate value
     def _set_numpick_value(self, delta: int):
         value = self._value + (delta * self._step)
         if self._auto_rotate:
@@ -186,7 +164,7 @@ class NumberPicker(Control):
         else:
             self._value = clamp(value, self.min_range, self._max_range)
 
-
+    # Internal function to check if mouse is over us.
     def _is_mouse_upon_me(self) -> bool:
         # If this returns False, mouse_leave event will triggered
         # Since, updown control is a combo of an edit and button controls...
@@ -199,106 +177,140 @@ class NumberPicker(Control):
         res = api.PtInRect(byref(self._my_rect), pt)
         return res
 
+    # Internal function to resize buddy edit
+    def _resize_buddy(self):
+        swp_flag = con.SWP_NOACTIVATE | con.SWP_NOZORDER
+        if self._btn_on_left:
+            api.SetWindowPos(self._buddy_hwnd, None,
+                            self._xpos + self._ud_rect.right,
+                            self._ypos,
+                            self._buddy_rect.right,
+                            self._buddy_rect.bottom, swp_flag)
+            self._linex = self._buddy_rect.left
+        else:
+            api.SetWindowPos(self._buddy_hwnd, None,
+                            self._xpos,
+                            self._ypos,
+                            self._buddy_rect.right - 2,
+                            self._buddy_rect.bottom, swp_flag)
+
+            self._linex = self._buddy_rect.right - 3
+
     # -endregion Private funcs
 
     # -region Properties
 
-    # @property
-    # def auto_size(self): return self._auto_size
-
-    # @Control.back_color.setter
-    # def back_color(self, value : int):
-    #     self._bg_color.update_color(value)
-    #     if not self._draw_flag & (1 << 1): self._draw_flag += 2
-    #     self._manage_redraw()
 
     @property
-    def decimal_points(self): return self._deci_precis
+    def decimal_points(self):
+        """Get the decimal points of NumberPicker"""
+        return self._deci_precis
 
     @decimal_points.setter
-    def decimal_points(self, value: int): self._deci_precis = value
+    def decimal_points(self, value: int):
+        """Set the decimal points of NumberPicker"""
+        self._deci_precis = value
     #-----------------------------------------------------------------------[1]
 
     @property
-    def min_range(self): return self._min_range
+    def min_range(self):
+        """Get minimum value of NumberPicker's range"""
+        return self._min_range
 
     @min_range.setter
     def min_range(self, value: int | float):
+        """Set minimum value of NumberPicker's range"""
         self._min_range = value
         if self._is_created:
             api.SendMessage(self._hwnd, con.UDM_SETRANGE32, self._min_range, self._max_range)
     #----------------------------------------------------------------------------------------[2]
 
     @property
-    def max_range(self): return self._max_range
+    def max_range(self):
+        """Get maximum value of NumberPicker's range"""
+        return self._max_range
 
     @max_range.setter
     def max_range(self, value: int | float):
+        """Set maximum value of NumberPicker's range"""
         self._max_range = value
         api.SendMessage(self._hwnd, con.UDM_SETRANGE32, self._min_range, self._max_range)
     #-----------------------------------------------------------------------------------[3]
 
     @property
-    def auto_rotate(self): return self._auto_rotate
+    def auto_rotate(self):
+        """Returns true if auto rotate is enabled. This means, it jumped from min value to max and vice versa."""
+        return self._auto_rotate
 
     @auto_rotate.setter
-    def auto_rotate(self, value: bool): self._auto_rotate = value
+    def auto_rotate(self, value: bool):
+        """Set true if auto rotate is enabled. This means, it jumped from min value to max and vice versa."""
+        self._auto_rotate = value
     #-----------------------------------------------------------------------[4]
 
     @property
-    def step(self): return self._step
+    def step(self):
+        """Get the step value of NumberPicker. Step is the amount of value jumped at one click."""
+        return self._step
 
     @step.setter
-    def step(self, value: int): self._step = value
+    def step(self, value: int):
+        """Set the step value of NumberPicker. Step is the amount of value jumped at one click."""
+        self._step = value
     #-----------------------------------------------------[5]
 
     @property
-    def value(self) -> float: return self._value
+    def value(self) -> float:
+        """Get the NumberPicker's value"""
+        return self._value
 
     @value.setter
     def value(self, value: float):
+        """Set the NumberPicker's value"""
         self._value = value
         if self._is_created:
             api.SetWindowText(self._buddy_hwnd, f"{self._value:.{self._deci_precis}f}")
     #----------------------------------------------------------------------------------[6]
 
-    # @property
-    # def hide_selection(self): return self._hide_sel ************NOT NEEDED***********
-
-    # @hide_selection.setter
-    # def hide_selection(self, value: bool):
-    #     self._hide_sel = value
-    #     if self._is_created: api.SendMessage(self._buddy_hwnd, con.EM_SETSEL, -1, 0)
-    # #-------------------------------------------------------------------------------[7]
 
     @property
-    def button_on_left(self): return self._btn_on_left
+    def button_on_left(self):
+        """Returns true if button is set on left side"""
+        return self._btn_on_left
 
     @button_on_left.setter
     def button_on_left(self, value: bool):
+        """Set true if button is set on left side"""
         self._btn_on_left = value
         if self._is_created: pass # api.SendMessage(self._buddy_hwnd, con.EM_SETSEL, -1, 0)
         # TODO : Change window style constants here to update the control.
     #-------------------------------------------------------------------[8]
 
     @property
-    def has_seperator(self): return self._has_sep
+    def has_seperator(self):
+        """Returns true if seperator is enabled"""
+        return self._has_sep
 
     @has_seperator.setter
     def has_seperator(self, value: bool):
+        """Set true if seperator is enabled"""
         self._has_sep = value
     #-----------------------------------------------------[9]
 
     @property
-    def hide_caret(self): return self._hide_caret
+    def hide_caret(self):
+        """Returns true if caret is hidden"""
+        return self._hide_caret
 
     @hide_caret.setter
     def hide_caret(self, value: bool):
+        """Set true to hide the caret"""
         self._hide_caret = value
     #-----------------------------------------------------[10]
 
     @Control.on_mouse_enter.setter
     def on_mouse_enter(self, value):
+        """Set mouse evnte event handler"""
         self._on_mouse_enter = value
         self._track_mouse_leave = True
     #--------------------------------------------[11]
@@ -306,24 +318,26 @@ class NumberPicker(Control):
 
     @Control.on_mouse_leave.setter
     def on_mouse_leave(self, value):
+        """Set mouse leave event handler"""
         self._on_mouse_leave = value
         self._track_mouse_leave = True
     #--------------------------------------------[12]
     # -endregion Properties
-    x = 100 # dummy
+
 
 #End NumberPicker
 
 
-# @WINFUNCTYPE(LRESULT, HWND, UINT, WPARAM, LPARAM, UINT_PTR, DWORD_PTR)
 @SUBCLASSPROC
 def np_wnd_proc(hw, msg, wp, lp, scID, refData) -> LRESULT:
-    # printWinMsg(msg)
+
     np = nump_dict[hw]
+    # log_msg(msg, f"Main proc {np.name}")
     match msg:
         case con.WM_DESTROY:
             api.RemoveWindowSubclass(hw, np_wnd_proc, scID)
-            # print("remove subclass for - ", np.name)
+            np._destroy_count += 1
+            if np._destroy_count == 2: del nump_dict[hw]
 
         case MyMessages.CTRL_NOTIFY:
             nm = cast(lp, api.LPNMUPDOWN).contents
@@ -357,12 +371,12 @@ def np_wnd_proc(hw, msg, wp, lp, scID, refData) -> LRESULT:
 def buddy_wnd_proc(hw, msg, wp, lp, scID, refData) -> LRESULT:
 
     np = nump_dict[refData]
-    # np.log("message : ", msg)
-    # log_msg(msg)
+    # log_msg(msg, np.name)
     match msg:
         case con.WM_DESTROY:
             api.RemoveWindowSubclass(hw, buddy_wnd_proc, scID)
-            # print("remove subclass for NumberPicker's Edit - ", np.name)
+            np._destroy_count += 1
+            if np._destroy_count == 2: del nump_dict[refData]
 
         case MyMessages.EDIT_COLOR:
             # Whether user selects a back color or not, we must set the back color.
@@ -394,7 +408,6 @@ def buddy_wnd_proc(hw, msg, wp, lp, scID, refData) -> LRESULT:
 
         case con.WM_KEYDOWN:
             np._key_pressed = True
-            # api.RedrawWindow(hw, None, None, con.RDW_INTERNALPAINT)
             np._key_down_handler(wp)
 
         case con.WM_KEYUP: np._key_up_handler(wp)
@@ -442,6 +455,8 @@ def buddy_wnd_proc(hw, msg, wp, lp, scID, refData) -> LRESULT:
             api.LineTo(hdc, np._linex, np._height - 1)
             api.ReleaseDC(hw, hdc)
             api.DeleteObject(fpen)
-            return 0
+            return 1
+        case MyMessages.BUDDY_RESET:
+            np._resize_buddy()
 
     return api.DefSubclassProc(hw, msg, wp, lp)
